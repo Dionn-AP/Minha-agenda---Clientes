e746d2cc125c03dca9ce4e55abf65af3d9bdf58b
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-12-02 21:49:22 -0300

    estilo do component GetStartedView

---
+++ src/components/GetStartedView/styles.ts
@@ -0,0 +1,39 @@
+import 'styled-components/native';
+import styled from 'styled-components/native';
+
+export const WhrapperMain = styled.View`
+    flex: 1; 
+    align-items: center; 
+    justify-content: flex-start;
+    background-color: #09184D;
+    padding: 0 10px;
+`
+export const TextContentTopSplash = styled.Text`
+    text-align: center;
+    color: #EDF2FA;
+    line-height: 28px;
+    letter-spacing: 0.2px;
+    font-size: 20px;
+    font-weight: 600;
+    margin-top: 70px;
+    margin-bottom: 120px;
+`
+
+export const ImageSplashOne = styled.Image`
+    height: 300px;
+    width: 360px;
+`
+
+export const TextButtonStarted = styled.Text`
+    color: #EDF2FA;
+    font-size: 16px;
+    font-weight: 600;
+`
+
+export const ButtonStarted = styled.TouchableOpacity`
+    text-align: center;
+    padding: 17px 95px;
+    border-radius: 8px; 
+    margin-top: 150px;
+    background-color: #7B5BF2;
+`
