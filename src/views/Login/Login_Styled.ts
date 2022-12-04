a8c40ec33935ca69a182bc251bd72bfcd23631ae
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-12-04 04:45:03 -0300

    criando a view Login

---
+++ src/views/Login/Login_Styled.ts
@@ -0,0 +1,87 @@
+import styled from 'styled-components/native';
+
+
+export const WrapperLogin = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background-color: #09184D;
+    padding: 0 25px;
+`
+
+export const FormLogin = styled.View`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+`
+
+export const TextTopLogin = styled.Text`
+    font-weight: 600;
+    font-size: 28px;
+    color: #EDF2FA;
+    margin-bottom: 90px;
+    letter-spacing: 1px;
+`
+
+export const InputsLogin = styled.TextInput`
+    width: 100%;
+    color: #808085;
+    background-color: #EDF2FA;
+    padding: 17px 11px 17px 48px;
+    border-radius: 8px;
+    margin-bottom: 29px;
+`
+
+export const ButtonLogin = styled.TouchableHighlight`
+    padding: 17px 0;
+    border-radius: 8px;
+    background-color: #7B5BF2;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 28px;
+`
+
+export const WrapperInputs = styled.View`
+    display: flex;
+    width: 100%;
+    height: auto;
+    position: relative;
+`
+
+export const ButtonInputs = styled.TouchableOpacity`
+    position: absolute;
+    top: 12px;
+    left: 8px;
+    opacity: 0.7;
+`
+
+export const ImagesInputs = styled.Image`
+    height: 30px;
+    width: 30px;
+`
+
+export const TextButtonLogin = styled.Text`
+    font-weight: 600;
+    font-size: 16px;
+    color: #EDF2FA;
+`
+
+export const TextForgot = styled.Text`
+    font-weight: 600;
+    font-size: 14px;
+    color: #EDF2FA;
+    margin-bottom: 53px;
+`
+
+export const WrapperTextBottomLogin = styled.View`
+
+`
+
+export const TexBottomLogin = styled.Text`
+    font-weight: 600;
+    font-size: 18px;
+    color: #EDF2FA;
+    letter-spacing: 0.1px;
+`
