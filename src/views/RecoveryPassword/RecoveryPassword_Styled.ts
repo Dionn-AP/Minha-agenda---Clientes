bddc18bf48e2f75a2726c34c6aa5a833a7ea86bc
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-12-06 05:52:34 -0300

    estilo da tela de Recuperar Senha

---
+++ src/views/RecoveryPassword/RecoveryPassword_Styled.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components/native';
+
+
+export const WrapperRecveryPassword = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: flex-start;
+    position: relative;
+    background-color: #09184D;
+    padding: 120px 25px 0 25px;
+`
+
+export const FormRecoveryPassword = styled.View`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+`
+
+export const TextTopRecoveryPassword = styled.Text`
+    font-weight: 600;
+    font-size: 28px;
+    color: #EDF2FA;
+    margin-bottom: 120px;
+    letter-spacing: 1px;
+`
+
+export const WrapperInputsRecoveryPassword = styled.View`
+    display: flex;
+    width: 100%;
+    height: auto;
+    position: relative;
+`
+
+export const InputsRecoveryPassword = styled.TextInput`
+    width: 100%;
+    color: #808085;
+    background-color: #EDF2FA;
+    padding: 17px 11px 17px 48px;
+    border-radius: 8px;
+    margin-bottom: 22px;
+`
+
+export const ButtonInputs = styled.TouchableOpacity`
+    position: absolute;
+    top: 12px;
+    left: 8px;
+    opacity: 0.7;
+`
+
+export const ButtonRecoveryPassword = styled.TouchableHighlight`
+    padding: 17px 0;
+    border-radius: 8px;
+    background-color: #7B5BF2;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 28px;
+`
+
+export const TextButtonRecoveryPassword = styled.Text`
+    font-weight: 600;
+    font-size: 16px;
+    color: #EDF2FA;
+`
+
+export const TexBottomRecoveryPassword = styled.Text`
+    font-weight: 600;
+    font-size: 18px;
+    color: #EDF2FA;
+    margin-bottom: 80px;
+    letter-spacing: 0.1px;
+`
+
+export const ButtonGoback = styled.TouchableOpacity`
+    position: absolute;
+    top: 15px;
+    left: 17px;
+`
