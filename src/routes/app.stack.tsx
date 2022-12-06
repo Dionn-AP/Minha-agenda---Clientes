8dbd27f9d0ca492c5187588e5251a9a8fdae3929
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-12-06 12:56:41 -0300

    adicionando a rota da tela Confirmar Cadastro

--- src/routes/app.stack.tsx
+++ src/routes/app.stack.tsx
@@ -4,6 +4,7 @@ import GetStarted from "../views/GetStarted/GetStarted";
 import Login from "../views/Login/Login";
 import Signup from "../views/Signup/Signup";
 import RecoveryPassword from "../views/RecoveryPassword/RecoveryPassword";
+import ConfirmRegister from "../views/ConfirmRegister/ConfirmRegister";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -17,6 +18,7 @@ export default function AppStack() {
             <Screen name="Login" component={Login} />
             <Screen name="Signup" component={Signup} />
             <Screen name="Recuperar Senha" component={RecoveryPassword} />
+            <Screen name="Confirmar Cadastro" component={ConfirmRegister} />
             <Screen name="Bem-Vindo" component={GetStarted} />
         </Navigator>
     );
