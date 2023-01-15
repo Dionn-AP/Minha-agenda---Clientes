7c52e2fc556a0483b9ac7376e312cc33506eedb3
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-15 02:34:08 -0300

    funcão de get do token headers

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -1,5 +1,14 @@
 
 
+export function getHeaders(iToken?: string) {
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${iToken}`
+      }
+    };
+    return headers;
+  }
+
 export const upperCaseName = (text: string) => {
     let upperText: string;
     upperText = text.substring(0, 1).toUpperCase() + text.substring(1, text.length).toUpperCase()
