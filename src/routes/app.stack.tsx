8352b84aa0d28a200bdd388709e7b1963bb5bc28
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-15 02:32:35 -0300

    distribuindo rotas

--- src/routes/app.stack.tsx
+++ src/routes/app.stack.tsx
@@ -1,10 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import GetStarted from "../views/GetStarted/GetStarted";
-import Login from "../views/Login/Login";
-import Signup from "../views/Signup/Signup";
-import RecoveryPassword from "../views/RecoveryPassword/RecoveryPassword";
-import ConfirmRegister from "../views/ConfirmRegister/ConfirmRegister";
 import Home from "../views/Home/Home";
 import MyAccount from "../views/MyAccount/MyAccount";
 import AboutApp from "../views/AboutApp/AboutApp";
@@ -24,10 +20,6 @@ export default function AppStack() {
                 headerShown: false,
             }}
         >
-            <Screen name="Login" component={Login} />
-            <Screen name="Signup" component={Signup} />
-            <Screen name="Recuperar Senha" component={RecoveryPassword} />
-            <Screen name="Confirmar Cadastro" component={ConfirmRegister} />
             <Screen name="Bem-Vindo" component={GetStarted} />
             <Screen name="Home" component={Home} />
             <Screen name="Minha Conta" component={MyAccount} />
