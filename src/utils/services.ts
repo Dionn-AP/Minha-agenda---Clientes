1a5997f635625e39c6a9858f9b248d1ab44cc1b5
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-24 23:41:45 -0300

    criação: funcao de validação de email

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -14,3 +14,5 @@ export const upperCaseName = (text: string) => {
     upperText = text.substring(0, 1).toUpperCase() + text.substring(1, text.length).toUpperCase()
     return upperText;
 }
+
+export const regexValidationEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
