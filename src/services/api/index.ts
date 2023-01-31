03b50dfccff1159ae309f18c077e720e1f322590
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-30 22:52:08 -0300

    mudança no enpoint de requisição da api

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -7,9 +7,12 @@ interface IAxios {
 }
 
 const api = axios.create({
-    baseURL: "https://server-my-schedule.onrender.com",
+    baseURL: "http://10.0.0.185:8000",
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' }
+    // baseURL: "https://server-my-schedule.onrender.com",
+    // timeout: 10000,
+    // headers: { 'Content-Type': 'application/json' }
 });
 
 export default api;
