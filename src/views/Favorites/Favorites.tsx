24525c8c8cd3d1a2b54adca8008a32febbfb6a11
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-30 22:53:16 -0300

    ''

--- src/views/Favorites/Favorites.tsx
+++ src/views/Favorites/Favorites.tsx
@@ -19,18 +19,21 @@ import {
     Octicons
 } from '@expo/vector-icons';
 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import IconUser from '../../assets/icon-user.svg';
+import IconClose from '../../assets/icon-close.svg';
 import api from '../../services/api';
 import { ICompanies } from '../../types';
 import { useAuth } from '../../context/Auth';
 import CardCompany from '../../components/CardCompany/CardCompany';
 import { ActivityIndicator, Alert } from 'react-native';
+import ViewServiceCompany from '../../components/ViewServiceCompany/ViewServiceCompany';
 
 
 export default function Favorites() {
     const nav = useNavigation();
     const { authData } = useAuth();
+    const [companyId, setCompanyId] = useState<string>("");
     const [openServices, setOpenServices] = useState<boolean>(false);
     const [loadingData, setLoadingData] = useState(true);
     const [companies, setCompanies] = useState<ICompanies[]>();
@@ -43,9 +46,14 @@ export default function Favorites() {
             setLoadingData(false);
         } catch (error: any) {
             setLoadingData(false);
-            return Alert.alert('Algo deu errado', 'Não foi possível carregar os dados. Por favor, recarregue o app')
+            return Alert.alert('Algo deu errado', 'Não foi possível carregar os dados. Por favor, recarregue o app');
         }
 
+    }
+
+    function ViewServies(idCompany: string) {
+        setCompanyId(idCompany);
+        setOpenServices(true);
     }
 
     useEffect(() => {
@@ -53,7 +61,7 @@ export default function Favorites() {
     }, [])
 
     return (
-        <SafeAreaView style={{ flex: 1, justifyContent: "flex-end", backgroundColor: "#09184D" }}>
+        <SafeAreaView style={{ flex: 1, justifyContent: "flex-start", backgroundColor: "#09184D" }}>
             <WrapperTop>
                 <ButtonGoback
                     onPress={() => nav.navigate("Home")}
@@ -64,54 +72,71 @@ export default function Favorites() {
                         color="#EDF2FA" />
                 </ButtonGoback>
                 <TextNameUser>{`Olá, ${authData?.name!.split(" ")[0]}`}</TextNameUser>
-                <ButtonOpacity
-                    onPress={() => nav.navigate("Minha Conta")}
-                >
-                    <IconUser
-                        style={styles.iconUser}
-                    />
-                </ButtonOpacity>
-            </WrapperTop>
-            <WrapperMain>
-                <TextCenterView>
-                    FAVORITOS
-                </TextCenterView>
                 {
-                    loadingData ?
-                        <ActivityIndicator
-                            color='#7B5BF2'
-                            size={80}
-                        />
+                    openServices ?
+                        <ButtonOpacity
+                            onPress={() => setOpenServices(false)}
+                            style={{ marginBottom: 10 }}
+                        >
+                            <IconClose
+                                style={styles.iconUser}
+                            />
+                        </ButtonOpacity>
                         :
-                        !companies?.length ?
-                            <WrapperEmptyContent>
-                                <TextNotFavorites>
-                                    Você ainda não favoritou nenhum serviço
-                                </TextNotFavorites>
-                                <ButtonForServices 
-                                onPress={() => nav.navigate("Serviços")}
-                                activeOpacity={0.7}
-                                >
-                                    <TextButtonForServices>IR PARA SERVIÇOS</TextButtonForServices>
-                                </ButtonForServices>
-                            </WrapperEmptyContent>
-
-                            : companies?.map((company, index) => {
-                                const isFavorite = company.id_favorite?.find(favoriteId => favoriteId.includes(authData?.id!))
-                                return (
-                                    <CardCompany
-                                        key={index}
-                                        idCompany={company._id!}
-                                        name={company.company}
-                                        favorite={isFavorite}
-                                        // setOpenServices={setOpenServices}
-                                        getAllCompanies={getAllCompanies}
-                                    />
-                                )
-                            })
-
+                        <ButtonOpacity
+                            onPress={() => nav.navigate("Minha Conta")}
+                        >
+                            <IconUser
+                                style={styles.iconUser}
+                            />
+                        </ButtonOpacity>
                 }
-            </WrapperMain>
+            </WrapperTop>
+            {
+                openServices ?
+                    <ViewServiceCompany
+                        companyId={companyId}
+                    />
+                    :
+                    <WrapperMain>
+                        <TextCenterView>
+                            FAVORITOS
+                        </TextCenterView>
+                        {loadingData ?
+                            <ActivityIndicator
+                                color='#7B5BF2'
+                                size={80}
+                            />
+                            :
+                            !companies?.length ?
+                                <WrapperEmptyContent>
+                                    <TextNotFavorites>
+                                        Você ainda não favoritou nenhum serviço
+                                    </TextNotFavorites>
+                                    <ButtonForServices
+                                        onPress={() => nav.navigate("Serviços")}
+                                        activeOpacity={0.7}
+                                    >
+                                        <TextButtonForServices>IR PARA SERVIÇOS</TextButtonForServices>
+                                    </ButtonForServices>
+                                </WrapperEmptyContent>
+
+                                : companies?.map((company, index) => {
+                                    const isFavorite = company.id_favorite?.find(favoriteId => favoriteId.includes(authData?.id!))
+                                    return (
+                                        <CardCompany
+                                            key={index}
+                                            idCompany={company._id!}
+                                            name={company.company}
+                                            favorite={isFavorite}
+                                            openServices={ViewServies}
+                                            getAllCompanies={getAllCompanies}
+                                        />
+                                    )
+                                })
+                        }
+                    </WrapperMain>
+            }
         </SafeAreaView>
     )
 };
